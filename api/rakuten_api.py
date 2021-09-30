7834d4b7f90734c2d62ab9f99bda7e0b9bb525cb
Yuta-Minami <s226264@wakayama-u.ac.jp>
2021-09-30 17:20:53 +0900

    GETで複数の値を取得可能に変更

--- api/rakuten_api.py
+++ api/rakuten_api.py
@@ -1,15 +1,18 @@
-from flask import Flask, jsonify
-from requests import request
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return jsonify({'message': 'Hello world'})
+# @app.route('/')
+# def hello_world():
+#     return jsonify({'message': 'Hello world'})
 
 @app.route('/', methods=["GET"])
 def get_user():
-    user = request.args.get("user")
+    input_url = request.args.getlist("input")
+    # 処理
+    input_food_list = input_url[0].split(',')
+    return jsonify(input_food_list[0])
+
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8880, debug=True)
